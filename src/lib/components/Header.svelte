42eaded950a5954d81154a1995dc66a245c6e1c7
Ben Chidgey <ben@feedmypixel.com>
2022-07-24 21:38:00 +0100

    Continuing setup of an app
    - Simple Header component
    - Setup TypeScript
    - Setup SASS
    - Setup Intellisense
    - Setup PostCSS
    - Add Global CSS
    - Add npm script to build global CSS from SASS
    - Add SASS work, mainly base styling and variables

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import feedMyPixelLogo from '$assets/images/feed-my-pixel.svg';
+</script>
+
+<header class="Header">
+  <div class="Header-primary">
+    <h1>feedMyPixel Ltd.</h1>
+  </div>
+  <div class="Header-secondary">
+    <img class="Header-logo" src={feedMyPixelLogo} alt="feedMyPixel Ltd." />
+  </div>
+</header>
+
+<style lang="scss">
+  @use '../assets/stylesheets/settings/spacing' as *;
+
+  .Header {
+    display: grid;
+    justify-content: center;
+    grid:
+      'primary secondary' 1fr
+      / 3fr 1fr;
+    gap: $spacing-16;
+    padding: 10px;
+  }
+
+  .Header-primary {
+    grid-area: primary;
+  }
+
+  .Header-secondary {
+    grid-area: secondary;
+    justify-self: end;
+  }
+
+  .Header-logo {
+    width: #{$spacing-40 * 2};
+    height: #{$spacing-40 * 2};
+  }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,79 @@
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import sveltePreprocess from 'svelte-preprocess';
+import autoprefixer from 'autoprefixer';
+import pxToRem from 'postcss-pxtorem';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess(),
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+const packageJson = JSON.parse(json);
+
+const config = defineConfig({
+  preprocess: sveltePreprocess({
+    typescript: true,
+    postcss: {
+      plugins: [
+        pxToRem({
+          rootValue: 16,
+          unitPrecision: 5,
+          selectorBlackList: [],
+          replace: true,
+          mediaQuery: true,
+          minPixelValue: 0,
+          propWhiteList: [
+            'font',
+            'font-size',
+            'line-height',
+            'letter-spacing',
+            'padding',
+            'padding-top',
+            'padding-right',
+            'padding-bottom',
+            'padding-left',
+            'margin',
+            'margin-top',
+            'margin-right',
+            'margin-bottom',
+            'margin-left',
+            'width',
+            'height',
+            'border-radius',
+            'letter-spacing'
+          ]
+        }),
+        autoprefixer
+      ]
+    }
+  }),
 
   kit: {
-    adapter: adapter()
+    alias: {
+      $components: 'src/lib/components',
+      $assets: 'src/lib/assets'
+    },
+
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false
+    }),
+
+    inlineStyleThreshold: 500,
+
+    version: {
+      name: packageJson?.version // TODO not too sure what this is doing?
+    },
+
+    prerender: {
+      // This can be false if you're using a fallback (i.e. SPA mode)
+      default: true
+    }
   }
-};
+});
 
 export default config;
